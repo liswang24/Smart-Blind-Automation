26ec1f952e71c32cd445e3fa28c4573672a03c0b
Lisa Wang <liswang24@gmail.com>
2021-08-11 21:49:41 -0700

    small fix for variable naming

--- Smart_Blind_Automation/pinout.h
+++ Smart_Blind_Automation/pinout.h
@@ -4,11 +4,11 @@
 #define MOTOR_IN2       25
 
 // Encoder Pins
-#define outputA         33
-#define outputB         32
+#define OUTPUT_A         33
+#define OUTPUT_B         32
 
 // Serial Monitor
-#define BAUDE_RATE      9600
+#define BAUD_RATE      9600
 
 // Motor Speed
 #define SPEED           128     // Slow = 64, Normal = 128, Fast = 255
